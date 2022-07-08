b346f740d3a82dca8e23f4db760c3dd00d8ba744
Divinefavour David <blazer0022@gmail.com>
2022-07-08 11:04:31 +0100

    tasks 1 and 2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * _isdigit - checks if arguement is digit.
+ * @c: arguement tested
+ * Return: 1 if uppercase, otherwise 0.
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multipliers two integer numbers and returns the result.
+ * @a: first arguement
+ * @b: second arguement
+ * Return: (0)
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
