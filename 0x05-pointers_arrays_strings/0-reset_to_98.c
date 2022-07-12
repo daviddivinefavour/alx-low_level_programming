1c865f6e3667ff075722b216c58f182b8f0b2c57
Divinefavour David <blazer0022@gmail.com>
2022-07-13 00:38:34 +0100

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+/**
+ * reset_to_98 -resets the value of a pointer to 98
+ * @n: arguement passed to function.
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void reset_to_98(int *n);
+char *_strcpy(char *dest, char *src);
+#endif /* MAIN_H */
+
